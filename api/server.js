2232071976dfb3cb3402458f51a1505cc6afdfda
Mevlutyildirim <Mevlutyildirim@users.noreply.github.com>
2020-07-12 12:37:57 +0300

    server has been constructed

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const App = express();
+const Cors = require("cors");
+const Logger = require("morgan");
+
+App.use(Cors());
+App.use(Logger());
+
+App.listen(5000, ()=> console.log("server is up and runing"));
+
