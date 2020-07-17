5655a608627a108380317de8d0291ee1549f24d9
Mevlutyildirim <Mevlutyildirim@users.noreply.github.com>
2020-07-17 17:19:38 +0300

    routing has been configured.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import SignUpScreen from "./screens/signUpScreen";
+
+function Main() {
+  return (
+    <Switch>
+      <Route path="/signIn" component={SignUpScreen} />
+    </Switch>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <Main />
     </div>
   );
 }
---
+++ src/screens/signUpScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function SignUpScreen(){
+
+  return(
+    <Card>
+
+    </Card>
+  )
+}
+
+const Card = styled.div`
+border: 1px solid #000;
+`;
+
+export default SignUpScreen;
