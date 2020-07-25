c44b3cdb1ad729fccbcc311561822d88e06eb66b
Mevlutyildirim <Mevlutyildirim@users.noreply.github.com>
2020-07-25 17:21:51 +0300

    task card has been added

--- src/screens/homeScreen.jsx
+++ src/screens/homeScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { styled } from 'styled-components';
+import styled  from 'styled-components';
+import Card from '../components/card';
 
 function HomeScreen(){
 
   return(
-  <div></div>
+  <Card/>
   )
 }
 
