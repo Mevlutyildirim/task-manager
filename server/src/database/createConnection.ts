6abf8944970452f132d321e9d6a466efcd644b2b
Mevlutyildirim <Mevlutyildirim@users.noreply.github.com>
2020-08-16 14:07:20 +0300

    eslint abosolute path bug fixes

--- server/src/database/createConnection.ts
+++ server/src/database/createConnection.ts
@@ -1,13 +1,16 @@
-import {createConnection, Connection} from 'typeorm';
-import * as entites from '../entity';
+import { createConnection, Connection } from "typeorm";
+import * as entites from "../entity";
 
-export const  connectDatabase =  ():Promise<Connection>=>  createConnection({
-    type:"mysql",
-    host:process.env.DB_HOST,
+const connectDatabase = (): Promise<Connection> =>
+  createConnection({
+    type: "mysql",
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: Object.values(entites),
-    synchronize:true
-});
+    synchronize: true,
+  });
+
+export default connectDatabase;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,31 @@
-import express from 'express';
-import Cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
+import express from "express";
 import "reflect-metadata";
-import {connectDatabase} from './database/createConnection';
+import Cors from "cors";
+import dotenv from "dotenv";
+import connectDatabase  from "./database/createConnection";
 
+dotenv.config();
 
-
-const boostDatabase = async ():Promise<void>=>{
-  
-  try{
+const boostDatabase = async (): Promise<void> => {
+  try {
     await connectDatabase();
-  }
-  catch(err){
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
-const boostExpress = ():void=>{
-  
+const boostExpress = (): void => {
   const App = express();
   App.use(Cors());
   App.use(express.json());
-  App.listen(5000, ()=> console.log(`server is up and running ${process.env.NODE_ENV}`));
-}
+  App.listen(5000, () =>
+    console.log(`server is up and running ${process.env.NODE_ENV}`)
+  );
+};
 
-const boostServer = async ():Promise<void>=>{
+const boostServer = async (): Promise<void> => {
   await boostDatabase();
   boostExpress();
-}
+};
 
 boostServer();
