e53686f221455b0f5ee1f910376249acba0073ee
Mevlutyildirim <Mevlutyildirim@users.noreply.github.com>
2020-08-25 17:59:31 +0300

    styles has separated

--- client/src/screens/signInScreen.tsx
+++ client/src/screens/signInScreen.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import {FormInput, SignTitle} from "shared/components";
+import React, { useState } from "react";
+import {FormInput, SignTitle, InputError} from "shared/components";
 import * as Button from "shared/components/button";
 import { useForm } from "react-hook-form";
 import {Main, Card} from 'screens/sign.style';
 
 const {PrimaryButton} = Button;
 function SignInScreen() {
+  const [isComplete, setComplete] = useState(false);
   const { register, handleSubmit, errors } = useForm();
   const onSumbit = (data:SignInFormProps) => {
     console.log(data);
@@ -16,16 +17,20 @@ function SignInScreen() {
       <SignTitle title="login"/>
       <FormInput
         placeholder="e-mail"
-        name="e-mail"
+        name="email"
+        id="email"
         type="e-mail"
-        ref={register({ required: true })}
+        ref={register({ required: "e-mail is required" })}
       />
+      {errors.email && <InputError>{errors.email.message}</InputError>}
       <FormInput
         placeholder="password"
         name="password"
+        id="password"
         type="password"
-        ref={register({ required: true })}
+        ref={register({ required: "password is required", minLength:6 })}
       />
+      {errors.password && <InputError>{errors.password.message}</InputError>}
       <PrimaryButton title="Sign In" />
     </Card>
     </Main>
---
+++ client/src/shared/styles/form.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const FormInput = styled.input`
+width: 324px;
+height: 40px;
+margin: 10px;
+background: #FFFFFF;
+border: 1px solid #DBDBDB;
+box-sizing: border-box;
+border-radius: 6px;
+padding-left:10px;
+`;
+
+export {FormInput};
